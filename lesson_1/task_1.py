997cf3832fea3479be8e65f400c4c467b61ec85f
Илья <primebox21@gmail.com>
2021-02-21 11:16:47 +0700

    lesson_1 complete
    Вроде всё сделал

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,23 +1,12 @@
 # Задание 1
 
-durations = [55, 114, 3756, 4865634, 157134324] # Промежутки времени в секундах
-for duration in durations:
-    # больше года
-    if (60 ** 3) * 24 * 31 < duration < (60 ** 3) * 24 * 31 * 12:
-        print(duration // (60 ** 3) * 24 * 31, 'лет', duration // (60 ** 3) * 24 * 31, 'мес', duration // (60 ** 3) * 24, 'сут', duration // 60 ** 2, 'час', duration // 60, 'мин', duration % 60, 'сек')
-    # до года
-    elif (60 ** 3) * 24 < duration < (60 ** 3) * 24 * 31:
-        print(duration // (60 ** 3) * 24, 'мес', duration // (60 ** 3) * 24, 'сут', duration // 60 ** 2, 'час', duration // 60, 'мин', duration % 60, 'сек')
-    # до месяца
-    elif 60 ** 3 < duration < (60 ** 3) * 24:
-        print(duration // 60 ** 3, 'сут', duration // 60 ** 2, 'часа', duration // 60, 'мин', duration % 60, 'сек')
-    # до суток
-    elif 60 ** 2 < duration < 60 ** 3:
-        print(duration // 60 ** 2, 'час', duration // 60, 'мин', duration % 60, 'сек')
-    # до часа
-    elif 60 < duration < 60 ** 2:
-        print(duration // 60, 'мин', duration % 60, 'сек')
-    # до минуты
-    else:
-        print(duration, 'сек')
+durations = [59, 62, 125, 3601]  # Промежутки времени в секундах
 
+for dur in durations:
+    if dur < 60:
+        print('duration =', dur)
+        print(dur, 'seconds\n')
+    else:
+        if 60 <= dur:
+            print('duration =', dur)
+            print(dur // 60, 'minutes', dur % 60, 'seconds\n')
--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -10,15 +10,11 @@ sum_result = 0
 
 for number in numbers:
     sum_div_7 = 0
-
     number = str(number)
-
     for item in number:
         item = int(item)
         sum_div_7 += item
-
     number = int(number)
-
     if sum_div_7 % 7 == 0:
         sum_result += number
 
@@ -29,15 +25,11 @@ sum_result = 0
 for number in numbers:
     sum_div_7 = 0
     number += 17
-
     number = str(number)
-
     for item in number:
         item = int(item)
         sum_div_7 += item
-
     number = int(number)
-
     if sum_div_7 % 7 == 0:
         sum_result += number
 
