150f2f4f6aea04f6bc33ae8f258a2f479fd59750
Илья <primebox21@gmail.com>
2021-02-26 21:39:35 +0700

    task_1 complete
    Выполнил задание 1

---
+++ lesson_2/task_1.py
@@ -0,0 +1,11 @@
+# Задание 1
+
+result_1 = 15 * 3
+result_2 = 15 / 3
+result_3 = 15 // 2
+result_4 = 15 ** 2
+
+print('15 * 3 = ', result_1, type(result_1))
+print('15 * / = ', result_2, type(result_2))
+print('15 // 2 = ', result_3, type(result_3))
+print('15 ** 2 = ', result_4, type(result_4))
