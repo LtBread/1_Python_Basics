1dfa523c9db8c1d8486c1c440584787901bd04b2
Илья <primebox21@gmail.com>
2021-02-26 20:58:06 +0700

    tast_2 changed
    Добавил там кое-что

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -6,8 +6,23 @@ for number in range(1000):
     if number % 2 != 0:
         numbers.append(number ** 3)
 
-sum_result = 0
 
+# Реализация без преобразования типа
+sum_result = 0
+for num in numbers:
+    rem = 0
+    integer = num
+    while integer != 0:
+        rem += integer % 10
+        integer = integer // 10
+        # print('Вывод:', num, integer, rem)
+    if rem % 7 == 0:
+        sum_result += num
+
+print('sum_result =', sum_result)
+# конец реализации
+
+sum_result = 0
 for number in numbers:
     sum_div_7 = 0
     number = str(number)
@@ -21,7 +36,6 @@ for number in numbers:
 print('sum_result =', sum_result)
 
 sum_result = 0
-
 for number in numbers:
     sum_div_7 = 0
     number += 17
@@ -34,3 +48,18 @@ for number in numbers:
         sum_result += number
 
 print('sum_result =', sum_result)
+
+# Реализация без преобразования типа
+sum_result = 0
+for num in numbers:
+    num += 17
+    rem = 0
+    integer = num
+    while integer != 0:
+        rem += integer % 10
+        integer = integer // 10
+    if rem % 7 == 0:
+        sum_result += num
+
+print('sum_result =', sum_result)
+# конец реализации
