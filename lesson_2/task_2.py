fdd090d24099f7ce4652026f2836cf7d7e08ebdb
Илья <primebox21@gmail.com>
2021-02-26 22:05:38 +0700

    task_2 start
    Начал выполнение второго задания

--- lesson_2/task_2.py
+++ lesson_2/task_2.py
@@ -3,3 +3,13 @@
 ex_list = ['в', '5', 'часов', '17', 'минут', 'температура',
            'воздуха', 'была', '+5', 'градусов']
 
+print(type(ex_list[0]))
+ex_list[0] = int(ex_list[0])
+print(type(ex_list[0]))
+
+print(type(ex_list[1]))
+ex_list[1] = int(ex_list[1])
+print(type(ex_list[1]))
+
+# for element in ex_list:
+#     print(element)
