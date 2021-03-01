f0d179143cd8b0d8b733f6ab47f2f97f7f68faab
Илья <primebox21@gmail.com>
2021-03-01 20:18:08 +0700

    update task_5

--- lesson_2/task_5.py
+++ lesson_2/task_5.py
@@ -1,6 +1,6 @@
 # Задание 5
 
-prices = [97.2, 54.37, 38.34, 76.7, 97.28, 53.5, 99.69, 40.99,
+prices = [97, 54.37, 38.34, 76.7, 97.28, 53.5, 99.69, 40.99,
           96.42, 51.38, 64.1, 92.53, 96.68, 78.19, 72.64, 34.16,
           80.66, 17.73, 54.15, 41.72]
 
@@ -9,7 +9,10 @@ print('По заданию А:')
 for idx in range(len(prices)):
     prices[idx] = str(prices[idx])
     price = prices[idx].split('.')
-    price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
+    if len(price) == 1:
+        price = f'{int(price[0]):.0f} руб {0:02d} коп'
+    else:
+        price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
     print(price, end='')
     if idx != len(prices) - 1:
         print(end=', ')
@@ -19,7 +22,10 @@ print(end='\n')
 print('По заданию B:')
 for idx in range(len(prices)):
     price = prices[idx].split('.')
-    price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
+    if len(price) == 1:
+        price = f'{int(price[0]):.0f} руб {0:02d} коп'
+    else:
+        price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
     print(price, end='')
     if idx != len(prices) - 1:
         print(end=', ')
@@ -29,9 +35,11 @@ print('Доказательство: id объекта до сортировки
 prices.sort()
 
 for idx in range(len(prices)):
-    price = str(prices[idx])
-    price = price.split('.')
-    price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
+    price = prices[idx].split('.')
+    if len(price) == 1:
+        price = f'{int(price[0]):.0f} руб {0:02d} коп'
+    else:
+        price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
     print(price, end='')
     if idx != len(prices) - 1:
         print(end=', ')
@@ -42,8 +50,12 @@ print('Доказательство: id объекта после сортиро
 print('По заданию C:')
 new_prices = sorted(prices, reverse=True)
 for idx in range(len(new_prices)):
+    new_prices[idx] = str(new_prices[idx])
     new_prices[idx] = new_prices[idx].split('.')
-    new_prices[idx] = f'{int(new_prices[idx][0]):.0f} руб {int(new_prices[idx][1]):02d} коп'
+    if len(new_prices[idx]) == 1:
+        new_prices[idx] = f'{int(new_prices[idx][0]):.0f} руб {0:02d} коп'
+    else:
+        new_prices[idx] = f'{int(new_prices[idx][0]):.0f} руб {int(new_prices[idx][1]):02d} коп'
     print(new_prices[idx], end='')
     if idx != len(new_prices) - 1:
         print(end=', ')
