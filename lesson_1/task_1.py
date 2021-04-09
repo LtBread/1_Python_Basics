c9498087e18c558a2494577613fba29427a054ca
Илья <primebox21@gmail.com>
2021-04-09 18:18:52 +0700

    task_1 shoveling
    перелопатил

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,12 +1,28 @@
-# Задание 1
+DUR_KEYS = ["сек", "мин", "час", "дн", "мес", "год (лет)"]
+DIVIDERS = [60, 60, 24, 31, 12, 100]
 
-durations = [59, 62, 125, 3601]  # Промежутки времени в секундах
 
-for dur in durations:
-    if dur < 60:
-        print('duration =', dur)
-        print(dur, 'seconds\n')
-    else:
-        if 60 <= dur:
-            print('duration =', dur)
-            print(dur // 60, 'minutes', dur % 60, 'seconds\n')
+def show_convert(interval):
+    print(f'{interval}:',
+          ' '.join([f'{dir_value} {dir_key}'
+                    for dir_key, dir_value in convert(interval)]))
+
+
+def convert(interval):
+    dur_list = []
+    for dur_key, divider in zip(DUR_KEYS, DIVIDERS):
+        if not interval % divider == 0:
+            dur_list.append([dur_key, interval % divider])
+        interval = interval // divider
+        if interval == 0:
+            break
+    dur_list.reverse()
+    return dur_list
+
+
+show_convert(249701)
+show_convert(1617235200)
+show_convert(1619827199)
+durations = [59, 62, 125, 3601]
+for duration in durations:
+    show_convert(duration)
---
+++ lesson_1/test.py
@@ -0,0 +1,27 @@
+# durations = [59, 62, 125, 3601]
+#
+# print(dir(durations))
+# durations.sort()
+# durations.reverse()
+# durations.remove(59)
+# durations.append(34)
+# ex = durations.pop(0)
+# print(ex)
+# durations.append(34)
+# durations.insert(1, 555)
+# print(durations.index(125))
+# durations.extend([76, 77, 344])
+# print(durations.count(77))
+# durations.clear()
+# print(durations)
+
+# print(3601 // 60)
+# print(3601 % 60)
+# dividers = [60, 60, 24, 31, 12]
+# dur_keys = ["сек", "мин", "час", "дн", "мес", "год (лет)"]
+# result = dict.fromkeys(dur_keys)
+# print(result)
+
+d = {'a': 1, 'b': 0, 'c': 3}
+d = list(d)
+print(d)
