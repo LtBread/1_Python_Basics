ad948808ec4fcee26f9f455ec10b9d8d730d4439
Илья <primebox21@gmail.com>
2021-04-12 09:49:59 +0700

    task_5 shoveling

--- lesson_2/second_run/task_5.py
+++ lesson_2/second_run/task_5.py
@@ -1,42 +1,27 @@
-# Задание 5
-
-prices = [97, 54.37, 38.34, 76.7, 97.28, 53.5, 99.69, 40.99,
-          96.42, 51.38, 64.1, 92.53, 96.68, 78.19, 72.64, 34.16,
-          80.66, 17.73, 54.15, 41.72, 120, 1540]
+PRICES = [97.55, 54.37, 38, 76.7, 36000, 53.5, 99.69, 40.99, 96.42, 51]
 
 
-def prices_show(list_prices):
-    for idx in range(len(list_prices)):
-        price = str(float(list_prices[idx]))
-        price = price.split('.')
-        price = f'{int(price[0]):.0f} руб {int(price[1]):02d} коп'
-        print(price, end='')
-        if idx != len(prices) - 1:
-            print(end=', ')
-    print(end='\n')
+def show_prices(list_prices):
+    list_prices = map(float, list_prices)
+    list_prices = map(str, list_prices)
+    result = [f"{int(price.split('.')[0])} руб {int(price.split('.')[1]):02d} коп"
+              for price in list_prices]
+    print(', '.join(result))
 
-
-# A
 
 print('По заданию А:')
-prices_show(prices)
+show_prices(PRICES)
 
-# B
+print('\n', 'По заданию B:')
+show_prices(PRICES)
+print('Доказательство: id объекта до сортировки:', id(PRICES))
+PRICES.sort()
+show_prices(PRICES)
+print('Доказательство: id объекта после сортировки:', id(PRICES))
 
-print('По заданию B:')
-prices_show(prices)
-print('Доказательство: id объекта до сортировки:', id(prices))
-prices.sort()
-prices_show(prices)
-print('Доказательство: id объекта после сортировки:', id(prices))
+print('\n', 'По заданию C:')
+new_prices = sorted(PRICES, reverse=True)
+show_prices(new_prices)
 
-# C
-
-print('По заданию C:')
-new_prices = sorted(prices, reverse=True)
-prices_show(new_prices)
-
-# D
-print('По заданию D:')
-new_prices = new_prices[:5]
-prices_show(new_prices)
+print('\n', 'По заданию D:')
+show_prices(new_prices[:5])
